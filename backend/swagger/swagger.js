1334576a2a9f1cef0aa3b76e9d96433a4f511566
Aryan Verma <aryanv2204@gmail.com>
2026-06-29 14:07:04 +0530

    update swagger

--- backend/swagger/swagger.js
+++ backend/swagger/swagger.js
@@ -7,28 +7,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const options = {
-    definition:{
-        openapi:"3.0.0",
-        info:{
-            title:"EV Charging Station API",
-            version:"1.0.0",
-            description:"REST API documentation for EV Charging Station Management System"},
-        servers:[{
-               url:"http://localhost:5000/api",
-                description:"Local server"
-            }],
-        components:{
-            securitySchemes:{
-            bearerAuth:{
-            type:"http",
-            scheme:"bearer",
-            bearerFormat:"JWT"
-        }}
-}
-},
-apis:[
-"./routes/*.js"
-]
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "EV Charging Station API",
+            version: "1.0.0",
+            description: "REST API documentation for EV Charging Station Management System"
+        },
+        servers: [
+            {
+                url: "http://localhost:5000",
+                description: "Local Server"
+            },
+            {
+                url: "https://evoltsoftassignment.onrender.com",
+                description: "Production Server"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT"
+                }
+            }
+        }
+    },
+    apis: [
+        "./routes/*.js"
+    ]
 };
 
 const swaggerSpec = swaggerJsdoc(options);
